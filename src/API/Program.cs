b17a01b2bdb88394cbdd59aa55858783426457b1
TexasDredd <cesar.em.arrioja@gmail.com>
2026-09-10 19:51:42 -0600

    feat(parcial01) - add db initializer

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Domain;
+using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()){ }
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedDataAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during database seeding.");        
+}
+   
 
 app.MapControllers();
 
